efe6d6a09f4f7d46b36e2d8a1f47bc9b92137f05
Alpha4210 <alpha.gamer.2427@gmail.com>
2025-04-05 14:05:25 +0530

    Added a feature to change to darkmode in the about page

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,6 +13,26 @@ export default function TextForm(props) {
         let newText = text.toLowerCase()
         setText(newText)
     }
+    const handleSentenceClick = () =>{
+        // console.log("Upper case was clicked" + text)
+        let newText = text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
+        setText(newText)  
+    }
+    const handleResetClick = () =>{
+        // console.log("Upper case was clicked" + text)
+        let newText = ''
+        setText(newText)
+    }
+    const handleTitleClick = () =>{
+        // console.log("Upper case was clicked" + text)
+        let newText = ''
+        let text_arr = text.split(' ')
+        for (let i = 0; i < text_arr.length; i++) {
+          const element = text_arr[i];
+          newText = newText  + element[0].toUpperCase() + element.slice(1) + ' '
+        }
+        setText(newText)
+    }
     const handleOnChange = (event) =>{
         // console.log("On change");
         setText(event.target.value);
@@ -34,6 +54,9 @@ export default function TextForm(props) {
       </div>
       <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to upper-case</button>
       <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to lower-case</button>
+      <button className="btn btn-primary mx-2" onClick={handleSentenceClick}>Convert to sentence-case</button>
+      <button className="btn btn-primary mx-2" onClick={handleTitleClick}>Convert to title-case</button>
+      <button className="btn btn-primary mx-2" onClick={handleResetClick}>Reset</button>
     </div>
     <div className="container my-3">
       <h1>Text summary</h1>
@@ -45,3 +68,5 @@ export default function TextForm(props) {
     </>
   );
 }
+
+//Exercise 1 - to enchance the app by adding more functions to the text.
